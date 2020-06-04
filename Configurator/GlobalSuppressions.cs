6eded500fe4b9229021f83df5341c03650962889
ForkBug <ForkBug@github.com>
2020-06-04 14:40:45 +0800

    fix some compile error

--- Configurator/GlobalSuppressions.cs
+++ Configurator/GlobalSuppressions.cs
@@ -7,3 +7,4 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Reliability", "CA2000", Justification = "Disposables are disposed elsewhere.", Scope = "module")]
 [assembly: SuppressMessage("Design", "CA1062", Justification = "Null-checks are performed elsewhere.", Scope = "module")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>")]
